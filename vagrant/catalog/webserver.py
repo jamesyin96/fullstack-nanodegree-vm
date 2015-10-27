050989d8940978639e2a894af310453c54a69975
jamesyin96 <jamesyin96@gmail.com>
2015-10-26 22:51:50 -0500

    update restaurantmenu webserver. It can display all restaurants and create new restaurant

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,31 +1,46 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            if self.path.endswith("/restaurant"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                engine = create_engine('sqlite:///restaurantmenu.db')
+                Base.metadata.bind = engine
+                DBSession = sessionmaker(bind=engine)
+                session = DBSession()
+                id_name_list = session.query(Restaurant).all()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                for id_name in id_name_list:
+                    output += "<p><b>%s</b>" % (id_name.name)
+                    output += '    <a href="/restaurant/%s/edit">edit</a>' % (id_name.id)
+                    output += '    <a href="/restaurant/%s/delete">delete</a><br/></p>' % (id_name.id)
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
-
-            if self.path.endswith("/hola"):
+            if self.path.endswith("/restaurant/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                engine = create_engine('sqlite:///restaurantmenu.db')
+                Base.metadata.bind = engine
+                DBSession = sessionmaker(bind=engine)
+                session = DBSession()
+                id_name_list = session.query(Restaurant).all()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "<h1> Add new restaurant </h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/new'><h2>Input new restaurant name:</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -44,11 +59,18 @@ class WebServerHandler(BaseHTTPRequestHandler):
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 messagecontent = fields.get('message')
+            engine = create_engine('sqlite:///restaurantmenu.db')
+            Base.metadata.bind = engine
+            DBSession = sessionmaker(bind=engine)
+            session = DBSession()
             output = ""
             output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
+            output += " <h2> Okay, new restaurant added </h2>"
             output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            new_restaurant = Restaurant(name=messagecontent[0])
+            session.add(new_restaurant)
+            session.commit()
+            output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/new'><h2>Input new restaurant name:</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
             self.wfile.write(output)
             print output
