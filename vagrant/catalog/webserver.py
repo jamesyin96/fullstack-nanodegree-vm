4f3c3704237d18be47378cb56ad6d99a53cb2a9b
jamesyin96 <jamesyin96@gmail.com>
2015-10-27 20:32:44 -0500

    Update the webserver get_Post method so it has better code simplicity

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,49 +1,46 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/restaurant"):
+            if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                engine = create_engine('sqlite:///restaurantmenu.db')
-                Base.metadata.bind = engine
-                DBSession = sessionmaker(bind=engine)
-                session = DBSession()
                 id_name_list = session.query(Restaurant).all()
                 output = ""
                 output += "<html><body>"
+                output += "<a href='/restaurants/new'>add new restaurant</a>"
                 for id_name in id_name_list:
                     output += "<p><b>%s</b>" % (id_name.name)
-                    output += '    <a href="/restaurant/%s/edit">edit</a>' % (id_name.id)
-                    output += '    <a href="/restaurant/%s/delete">delete</a><br/></p>' % (id_name.id)
+                    output += '    <a href="/restaurants/%s/edit">edit</a>' % (id_name.id)
+                    output += '    <a href="/restaurants/%s/delete">delete</a><br/></p>' % (id_name.id)
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
-            if self.path.endswith("/restaurant/new"):
+            if self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                engine = create_engine('sqlite:///restaurantmenu.db')
-                Base.metadata.bind = engine
-                DBSession = sessionmaker(bind=engine)
-                session = DBSession()
-                id_name_list = session.query(Restaurant).all()
                 output = ""
                 output += "<html><body>"
                 output += "<h1> Add new restaurant </h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/new'><h2>Input new restaurant name:</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><h2>Input new restaurant name:</h2><input name="newRestaurantName" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
 
         except IOError:
@@ -51,29 +48,22 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            engine = create_engine('sqlite:///restaurantmenu.db')
-            Base.metadata.bind = engine
-            DBSession = sessionmaker(bind=engine)
-            session = DBSession()
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, new restaurant added </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            new_restaurant = Restaurant(name=messagecontent[0])
-            session.add(new_restaurant)
-            session.commit()
-            output += '''<form method='POST' enctype='multipart/form-data' action='/restaurant/new'><h2>Input new restaurant name:</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # create new restaurant and add to database
+                    new_restaurant = Restaurant(name=messagecontent[0])
+                    session.add(new_restaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
         except:
             pass
 
